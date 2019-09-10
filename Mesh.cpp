959046cdb5336cf139037a2f3554e488b274d4f8
2KYan <2kyan@outlook.com>
2019-09-09 20:27:26 -0700

    Update Tests

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,4 +1,5 @@
 #include "Mesh.h"
+#include "glad/glad.h"
 
 Mesh::Mesh()
 {
@@ -6,4 +7,27 @@ Mesh::Mesh()
 
 Mesh::~Mesh()
 {
+}
+
+void Mesh::setupMesh()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), ( void*) offsetof(Vertex, Position));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), ( void*) offsetof(Vertex, Normal));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), ( void*) offsetof(Vertex, TexCoords));
+
+    glBindVertexArray(0);
 }
